54d6fba6d0bd3a62a5c57fac645a97ac710e0fa9
chin-ninn <standbloom1220@gmail.com>
2023-10-29 17:49:30 +0900

    写真投稿ページの実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -23,20 +23,35 @@ class PhotosController < ApplicationController
   end
 
   def edit
-    
+    @photo = Photo.find(params[:id])
+    #if current_user.if != @photo.user_id
+      #redirect_to action: :index
+    #end
   end
 
   def update
+    @photo = Photo.find(params[:id])
+    @photo.update(photo_params)
     
+    if @photo.save
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
   end
 
   def destroy
-    
+    @photo = Photo.find(params[:id])
+    #if current_user.id == @photo.user_id
+    @photo.destroy
+    #end
+    redirect_to action: :index
   end
 
   private
 
   def photo_params
     params.require(:photo).permit(:photo_text)#.merge(user_id: current_user.id)
+    #active strage実装後:imageをPermitに追加する
   end
 end

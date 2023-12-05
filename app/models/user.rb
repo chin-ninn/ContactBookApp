a56595dba82167f9354e2a747eac462427f0c46d
chin-ninn <standbloom1220@gmail.com>
2023-12-05 21:02:49 +0900

    userのバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :notices, dependent: :destroy
-  has_many :photos, dependent: :destroy
-  has_many :calendars, dependent: :destroy
+  belongs_to :admin
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
@@ -24,16 +22,16 @@ class User < ApplicationRecord
   
   validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
   validates :prefecture_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
-  validates :city,presence: true
-  validates :house_number,presence: true
+  validates :city, presence: true
+  validates :house_number, presence: true
 
   validates :phone_number, presence: true, numericality: { only_integer: true, message: 'use single-byte number' }
   validates :phone_number, length: { maximum: 11 }
   validates :phone_number, length: { minimum: 10 }
   
-  validates :phone_number, numericality: { only_integer: true, message: 'use single-byte number' }
-  validates :phone_number, length: { maximum: 11 }
-  validates :phone_number, length: { minimum: 10 }
+  validates :fax_number, numericality: { only_integer: true, message: 'use single-byte number' }
+  validates :fax_number, length: { maximum: 11 }
+  validates :fax_number, length: { minimum: 10 }
 
-  
+  validates :admin_id, presence: true
 end
